06a3dbb9e229240e8bdeb8faaa9bfc9946d31c43
srmadhav <madhavrajput.1996@gmail.com>
2020-06-04 12:55:04 +0530

    Add about text and change images and solve error where forecast = forcecast

--- src/app.js
+++ src/app.js
@@ -49,18 +49,18 @@ app.get('/weather', (req, res) => {
        })
     }
    
-  geocode(req.query.address, (error,{latitude, longitude, location } = {} ) => {
+  geocode(req.query.address, (error,{ latitude, longitude, location } = {} ) => {
       if (error) {
-          return res.send({error })
+          return res.send({ error })
       }
 
-      forcecast(latitude, longitude, (error, forcecastData) => {
+      forcecast(latitude, longitude, (error, forecastData) => {
           if (error) {
-              return res.send({ error})
+              return res.send({ error })
           }
 
           res.send({
-              forcecast: forcecastData,
+              forecast: forecastData,
               location,
               address: req.query.address
           })
